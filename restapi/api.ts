a9fc9c925a632dc2cf7e90a118018726734a557a
Pablo Alonso <uo277931@uniovi.es>
2022-03-11 20:26:46 +0100

    Cambiada configuracion cloudinary

--- restapi/api.ts
+++ restapi/api.ts
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import "./database";
-import "./cloudinary"
 import user from "./routes/user";
 import product from "./routes/product";
 const api: Router = Router();
--- restapi/cloudinary.ts
+++
@@ -1,4 +0,0 @@
-require('dotenv').config();
-const cloudinary = require('cloudinary').v2
-
-cloudinary.config().CLOUDINARY_URL;
