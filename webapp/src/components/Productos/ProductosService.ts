756018863091e3564901952210c416bf7d4b8ecf
Pablo Alonso <uo277931@uniovi.es>
2022-04-02 11:05:17 +0200

    Cambio conexión con api

--- webapp/src/components/Productos/ProductosService.ts
+++ webapp/src/components/Productos/ProductosService.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const getProductos = async() => {
-   return await axios.get("http://localhost:5000/api/product/list")
+   return await axios.get(process.env.REACT_APP_API_URI || 'http://localhost:5000/api')
 }
--- webapp/src/components/Users/UserService.ts
+++ webapp/src/components/Users/UserService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import {User} from "./User";
 
-const API = 'http://localhost:5000/api';
+const API = process.env.REACT_APP_API_URI || 'http://localhost:5000/api';
 
 export const createNewUser = async (user: User) => {
     return await axios.post(`${API}/signup`, user);
