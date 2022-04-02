100386216fafd71019c42e7922e56e0b132c3605
sergiomalv <sermual@gmail.com>
2022-04-02 19:06:24 +0200

    Test básico de productos

--- restapi/tests/api.test.ts
+++ restapi/tests/api.test.ts
@@ -4,6 +4,7 @@ import * as http from 'http';
 import bp from 'body-parser';
 import cors from 'cors';
 import user from "../routes/user";
+import product from '../routes/product';
 import mongoose from 'mongoose';
 
 let app:Application;
@@ -23,6 +24,7 @@ beforeAll(async () => {
     app.use(cors(options));
     app.use(bp.json());
     app.use(user);
+    app.use(product);
 
     server = app.listen(port, ():void => {
         console.log('Restapi server for testing listening on '+ port);
@@ -33,9 +35,12 @@ beforeAll(async () => {
 
 afterAll(async () => {
     await mongoose.connection.collections['users'].drop();
+    await mongoose.connection.collections['products'].drop();
     await mongoose.connection.close();
     server.close() //close the server
 });
+
+//------------------------------USERS------------------------------
 
 describe('user ', () => {
     /**
@@ -66,5 +71,25 @@ describe('user ', () => {
         }
         const response:Response = await request(app).post('/signup').send(user).set('Accept', 'application/json');
         expect(response.statusCode).toBe(400);
+    });
+});
+
+//------------------------------PRODUCTS------------------------------
+describe('products ', () => {
+    /**
+     * Tests that a user can be created.
+     */
+    it('can be created correctly', async () => {
+        const product = {
+            name: "Laptop",
+            description: "Simple laptop",
+            basePrice: 1345,
+            IVA: 0.21,
+            units: 4,
+            categories: ["Electronic", "Laptop"],
+            urlImage: "https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE4LqQX?ver=1f00"
+        }
+        const response:Response = await request(app).post('/product/add').send(product).set('Accept', 'application/json');
+        expect(response.statusCode).toBe(200);
     });
 });
