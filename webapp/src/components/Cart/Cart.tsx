f58df302710b2090f79507fdc09f34e6d211e411
UO270157 <uo270157@uniovi.es>
2022-04-27 00:40:41 +0200

    Precio total arreglado

--- webapp/src/components/Cart/Cart.tsx
+++ webapp/src/components/Cart/Cart.tsx
@@ -13,8 +13,17 @@ const Carrito = (props: Products) => {
   const [price, setPrice] = useState<number>(0);
   const navigate = useNavigate();
 
+  const calculateTotalPrice = () =>{
+    setPrice(0);
+    let total = 0;
+    for(var i = 0;i<props.products.length;i++){
+        total += props.products[i].producto.price * props.products[i].num;
+    }
+    setPrice(total);
+  }
+
   useEffect(()=>{
-    props.products.forEach(item => setPrice(price+ item.producto.price*item.num));
+    calculateTotalPrice();
   }, [])
 
   const removeFromCart = (producto: Item)=>{
@@ -28,6 +37,7 @@ const Carrito = (props: Products) => {
             }
         }
     });
+    calculateTotalPrice();
   }
 
   const createOrder = ()=>{
@@ -38,7 +48,7 @@ const Carrito = (props: Products) => {
 
   return (
     <div>
-      <h1 className='title'>Mi carrito</h1>
+      <h1 className='title'>My cart</h1>
       <div className='productos'>
         {props.products.map(item => 
           {
@@ -52,7 +62,7 @@ const Carrito = (props: Products) => {
                   </div>
         )})}
       </div>
-      <h2>Total: {price}</h2>
+      <h2>Total: {price}€</h2>
       <button onClick={()=>createOrder()} disabled={props.products.length==0}>Finalizar pedido</button>
     </div>
   )
